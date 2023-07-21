bae304fe919a34b33bc1894c0f108f3ae96f6226
Michael Maia <contatomichaelmaia@gmail.com>
2023-07-21 19:07:03 -0300

    Update Forms Menu

--- test/specs/start.spec.js
+++ test/specs/start.spec.js
@@ -1,15 +1,21 @@
 
 describe('Primeiro test', () => {
 
-    it.only('acessar o menu forms', async () => {
-        await $('~Forms').click() 
+    it('acessar o menu forms', async () => {
+        //Forms
+        await $('~Forms').click()
+        await $('android=new UiSelector().text("Input field:")').waitForExist({timeout: 20000})  
+        //Input field:
         await $('~text-input').setValue('Michael QA')
+        //Switch:
         await $('~switch').click()
+        //Dropdown:
         const button = await $('~Dropdown')
         await button.click()
-        await $('android.widget.CheckedTextView').click()
-
+        await $('android=new UiSelector().text("Appium is awesome")').click()
+        //Button Active:
         await $('~button-Active').click()
+    
 
         expect(await $('~text-input')).toBeDisplayed()
     });
