b33c5c95604e5b8316f514ba71b3e9c11ed22030
Michael Maia <contatomichaelmaia@gmail.com>
2023-07-21 18:26:00 -0300

    Custom Forms

--- test/specs/start.spec.js
+++ test/specs/start.spec.js
@@ -1,9 +1,18 @@
+
 describe('Primeiro test', () => {
 
     it.only('acessar o menu forms', async () => {
-        await $('~Forms').click()
-        await $('android=new UiSelector().text("Input field:")').waitForExist({timeout: 20000})
-        await $('~text-input').setValue('teste')
+        await $('~Forms').click() 
+        await $('~text-input').setValue('Michael QA')
+        await $('~switch').click()
+        const button = await $('~Dropdown')
+        await button.click()
+        await $('android.widget.CheckedTextView').click()
+
+        await $('~button-Active').click()
+
+
+
         expect(await $('~text-input')).toBeDisplayed()
     });
 
